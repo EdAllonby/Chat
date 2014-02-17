2c0309c95de7cdee6a6c7a0fe2de4d6d603bfa58
Ed Allonby <ed.allonby@blueyonder.co.uk>
2014-02-17 23:08:13 +0000

    Added implementation to BinaryFormat.Serialise. Removed useless ILogger interface.  Added workable Client program which gives username and message and sends them out.

--- Server/ISubject.cs
+++ Server/ISubject.cs
@@ -2,8 +2,21 @@
 {
     public interface ISubject
     {
+        /// <summary>
+        /// Subscribe an object to the subject object who wants to receive notifications of changes to the subject
+        /// </summary>
+        /// <param name="o">Object that wants to be notified from the subject</param>
         void RegisterObserver(IObserver o);
+        
+        /// <summary>
+        /// Remove an object from the subject to no longer receive notifications
+        /// </summary>
+        /// <param name="o">Object that wants to be removed from the subscription list</param>
         void RemoveObserver(IObserver o);
+        
+        /// <summary>
+        /// Notify all subscribed observers of changes to the subject
+        /// </summary>
         void NotifyObservers();
     }
 }
--- Server/Serialisation/ITcpSendBehaviour.cs
+++ Server/Serialisation/ITcpSendBehaviour.cs
@@ -1,7 +1,16 @@
-﻿namespace Server
+﻿namespace Server.Serialisation
 {
     public interface ITcpSendBehaviour
     {
-        Client Serialise();
+        /// <summary>
+        /// Method used to serialise the object data over the TCP Socket
+        /// </summary>
+        void Serialise(Client clientMessage);
+
+        /// <summary>
+        /// Method to get incoming data and deserialise it into a Client object
+        /// </summary>
+        /// <returns>A Client object</returns>
+        Client Deserialise();
     }
 }
