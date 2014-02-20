3866115a7f7df94e21e2e89182c63b086e97fe25
Ed Allonby <ed.allonby@blueyonder.co.uk>
2014-02-20 22:48:58 +0000

    Cleaned ChatProgram methods to be more readable and easier to follow.

--- ChatProgram/Program.cs
+++ ChatProgram/Program.cs
@@ -8,48 +8,63 @@ namespace ChatProgram
 {
     internal static class Program
     {
-        private static TcpClient client = null;
+        private static TcpClient client;
 
-        private static string name;
-        private static string message;
+        private static string clientName;
+        private static string clientMessage;
 
         private static ITcpSendBehaviour sendBehaviour;
 
         private static void Main()
         {
-            sendBehaviour = new BinaryFormat();
+            Client joinedClient = InitialiseClientProgram();
 
-            Console.WriteLine("What is your name");
-            name = Console.ReadLine();
-
-            while (string.IsNullOrEmpty(name))
-            {
-                Console.WriteLine("You have to specify a name");
-                name = Console.ReadLine();
-            }
-
-            var client = new Client(name);
-
-            SendMessage(client);
-
-            string file = @"C:\test.txt";
+            SendMessage(joinedClient);
 
             do
             {
                 Console.Write("Chat: ");
 
-                message = Console.ReadLine();
+                Program.clientMessage = Console.ReadLine();
 
-                var clientMessage = new Client(name, message);
+                var clientMessage = new Client(clientName, Program.clientMessage);
                 SendMessage(clientMessage);
             } while (true);
+        }
+
+        private static Client InitialiseClientProgram()
+        {
+            SetBehaviour(new BinaryFormat());
+
+            SetClientName();
+
+            var joinedClient = new Client(clientName);
+
+            return joinedClient;
+        }
+
+        private static void SetBehaviour(ITcpSendBehaviour wantedSendBehaviour)
+        {
+            sendBehaviour = wantedSendBehaviour;
+        }
+
+        private static void SetClientName()
+        {
+            Console.WriteLine("What is your name");
+            clientName = Console.ReadLine();
+
+            while (string.IsNullOrEmpty(clientName))
+            {
+                Console.WriteLine("You have to specify a name");
+                clientName = Console.ReadLine();
+            }
         }
 
         private static void SendMessage(Client clientMessage)
         {
             try
             {
-                client = new TcpClient("localhost", ServerData.portNumber);
+                client = new TcpClient("localhost", ServerData.PortNumber);
                 Stream networkStream = client.GetStream();
 
                 sendBehaviour.Serialise(networkStream, clientMessage);
@@ -72,7 +87,7 @@ namespace ChatProgram
             {
                 if (client != null)
                 {
-                    client.Close();    
+                    client.Close();
                 }
             }
         }
