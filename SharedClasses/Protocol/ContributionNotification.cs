4a5431e4d8606f361c6441d0e488c6307312657c
Ed Allonby <ed.allonby@blueyonder.co.uk>
2014-03-24 22:38:06 +0000

    Added barebones for contribution protocol and serialisation.

---
+++ SharedClasses/Protocol/ContributionNotification.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using SharedClasses.Domain;
+
+namespace SharedClasses.Protocol
+{
+    [Serializable]
+    internal class ContributionNotification
+    {
+        public Contribution Contribution { get; set; }
+    }
+}
