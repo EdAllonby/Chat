b2d825e81698f0ebd772f84149318b055f45fc0d
Ed Allonby <edward.allonby@gmail.com>
2014-03-25 19:57:27 +0000

    Added barebones for the LoginRequest Protocol. Removed unused references to solution too.

---
+++ SharedClasses/Protocol/LoginRequestSerialiser.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using log4net;
+
+namespace SharedClasses.Protocol
+{
+    public class LoginRequestSerialiser
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof (LoginRequestSerialiser));
+
+        private readonly BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+        public void Serialise(LoginRequest loginRequest, NetworkStream networkStream)
+        {
+            try
+            {
+                if (networkStream.CanWrite)
+                {
+                    Log.Info("Attempt to serialise LoginRequest and send to stream");
+                    binaryFormatter.Serialize(networkStream, loginRequest);
+                    Log.Info("LoginRequest serialised and sent to network stream");
+                }
+            }
+            catch (IOException ioException)
+            {
+                Log.Error("connection lost between the client and the server", ioException);
+            }
+        }
+
+        public LoginRequest Deserialise(NetworkStream networkStream)
+        {
+            try
+            {
+                if (networkStream.CanRead)
+                {
+                    var loginRequest = (LoginRequest) binaryFormatter.Deserialize(networkStream);
+                    Log.Info("Network stream has received data and deserialised to a LoginRequest object");
+                    return loginRequest;
+                }
+            }
+            catch (IOException ioException)
+            {
+                Log.Error("connection lost between the client and the server", ioException);
+                networkStream.Close();
+            }
+            return new LoginRequest();
+        }
+    }
+}
