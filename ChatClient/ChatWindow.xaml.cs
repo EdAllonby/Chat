df85c172476f30bbd521426818a9958610d760f2
Ed Allonby <edward.allonby@gmail.com>
2014-04-13 00:54:04 +0100

    Implemented ChatWindow code to send and receive chat messages.

--- ChatClient/ChatWindow.xaml.cs
+++ ChatClient/ChatWindow.xaml.cs
@@ -1,13 +1,55 @@
-﻿namespace ChatClient
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using SharedClasses.Domain;
+
+namespace ChatClient
 {
     /// <summary>
     ///     Interaction logic for ChatWindow.xaml
     /// </summary>
     public partial class ChatWindow
     {
+        private readonly Client client;
+
         public ChatWindow()
         {
+            client = Client.GetInstance();
+            client.OnNewContributionNotification += UpdateChatTextBlock;
+            client.OnNewUser += UpdateUserList;
             InitializeComponent();
+            TitleLabel.Content = "Hello " + client.UserName;
+        }
+
+        private void UpdateUserList(IList<User> users, EventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() =>
+            {
+                ConnectedUsersListBox.Items.Clear();
+                foreach (User user in users)
+                {
+                    ConnectedUsersListBox.Items.Add(user.UserName);
+                }
+            });
+        }
+
+        private void UpdateChatTextBlock(string contribution, EventArgs e)
+        {
+            Application.Current.Dispatcher.Invoke(() => { ChatTextBlock.Items.Add(contribution); });
+        }
+
+        private void SendNotificationRequestButton_Click(object sender, RoutedEventArgs e)
+        {
+            client.SendContributionRequestMessage(ContributionRequestTextBox.Text);
+            ContributionRequestTextBox.Text = "Type your message...";
+        }
+
+        private void EnterTextBox(object sender, RoutedEventArgs e)
+        {
+            if (ContributionRequestTextBox.Text == "Type your message...")
+            {
+                ContributionRequestTextBox.Text = string.Empty;
+            }
         }
     }
 }
