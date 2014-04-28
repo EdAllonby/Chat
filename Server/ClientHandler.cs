1789bec683d6f73dc1477bb883230713f487bf48
Ed Allonby <edward.allonby@gmail.com>
2014-04-28 23:50:47 +0100

    Cleanup of Server and ClientHandler.

--- Server/ClientHandler.cs
+++ Server/ClientHandler.cs
@@ -12,7 +12,7 @@ namespace Server
     /// The Client handler is in charge of abstracting away the TcpClient Network work from the <see cref="Server"/>
     /// This class has no logic other than to send and receive messages. The logic is handled by <see cref="Server"/>
     /// </summary>
-    public sealed class ClientHandler
+    public sealed class ClientHandler : IDisposable
     {
         private static readonly ILog Log = LogManager.GetLogger(typeof (ClientHandler));
 
@@ -20,17 +20,21 @@ namespace Server
         private readonly SerialiserFactory serialiserFactory = new SerialiserFactory();
 
         private readonly TcpClient tcpClient;
+        private readonly User clientUser;
 
-        private int totalListenerThreads;
+        private static int totalListenerThreads;
 
-        public ClientHandler(TcpClient client)
+        public ClientHandler(User user, TcpClient client)
         {
             tcpClient = client;
+            clientUser = user;
             Log.Info("New client handler created");
         }
 
-        public User ClientUser { get; private set; }
-
+        public User ClientUser
+        {
+            get { return clientUser; }
+        }
 
         public event EventHandler<MessageEventArgs> OnNewMessage
         {
@@ -47,26 +51,16 @@ namespace Server
             messageListenerThread.Start();
         }
 
-        public LoginRequest GetClientLoginCredentials()
-        {
-            var messageIdentifierSerialiser = new MessageIdentifierSerialiser();
-            int messageIdentifier = messageIdentifierSerialiser.DeserialiseMessageIdentifier(tcpClient.GetStream());
-            ISerialiser serialiser = serialiserFactory.GetSerialiser(messageIdentifier);
-            var loginRequest = (LoginRequest) serialiser.Deserialise(tcpClient.GetStream());
-            return loginRequest;
-        }
-
-        public void AddUserToClientHandler(User user)
-        {
-            ClientUser = user;
-        }
-
         public void SendMessage(IMessage message)
         {
             ISerialiser messageSerialiser = serialiserFactory.GetSerialiser(message.Identifier);
-            TcpClient client = tcpClient;
-            messageSerialiser.Serialise(message, client.GetStream());
+            messageSerialiser.Serialise(message, tcpClient.GetStream());
             Log.Debug("Sent message with identifier " + message.Identifier + " to user with id " + ClientUser.UserId);
+        }
+
+        public void Dispose()
+        {
+            tcpClient.Close();
         }
     }
 }
