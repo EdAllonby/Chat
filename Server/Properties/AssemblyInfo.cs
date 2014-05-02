bd7fdd6df1ebaaf512424ca8977d0431dbff59ec
Ed Allonby <ed.allonby@blueyonder.co.uk>
2014-05-02 22:24:33 +0100

    Replaced Server with a Server Service.

--- Server/Properties/AssemblyInfo.cs
+++ Server/Properties/AssemblyInfo.cs
@@ -1,33 +1,32 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using log4net.Config;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+using log4net.Config;
 
-[assembly: AssemblyTitle("AllChatProgramProjects")]
+[assembly: AssemblyTitle("Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AllChatProgramProjects")]
+[assembly: AssemblyProduct("Server")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Set the project to watch the log4net.config file and reload of the config changes when needed.
-
-[assembly: XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8116a546-b7f2-4248-bfef-52c95bb77cae")]
 
-[assembly: Guid("70819214-73d3-4e3f-8db0-7304ae0b7907")]
+// Set the project to watch the log4net.config file and reload of the config changes when needed.
+
+[assembly: XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -39,6 +38,5 @@ using log4net.Config;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Server/ServerService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace Server
+{
+    public partial class ServerService : ServiceBase
+    {
+        public ServerService()
+        {
+            InitializeComponent();
+        }
+
+        public void OnDebug()
+        {
+            OnStart(null);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            var serverThread = new Thread(StartServer);
+            serverThread.Start();
+        }
+
+        private void StartServer()
+        {
+            var Server = new Server();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
