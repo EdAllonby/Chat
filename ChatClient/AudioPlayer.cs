99c45f5f3778f1613dc28263ad2545bc3f03db0a
Ed Allonby <edward.allonby@gmail.com>
2014-05-03 13:24:53 +0100

    Added audio when contribution is received.

---
+++ ChatClient/AudioPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Media;
+
+namespace ChatClient
+{
+    public class AudioPlayer : IAudioPlayer
+    {
+        private readonly SoundPlayer player = new SoundPlayer();
+
+        public void Play(Stream resource)
+        {
+            using (resource)
+            {
+                player.Stream = resource;
+                player.Play();
+            }
+        }
+    }
+}
