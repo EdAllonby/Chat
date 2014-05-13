3374690fba2a4fd7bafe63ce2af56e600f7c95b2
Ed Allonby <edward.allonby@gmail.com>
2014-05-13 22:20:44 +0100

    Added UserMessage to MVVM model. This makes the domain model cleaner as less view related clutter is in the Contribution class, and view specific data is held in the UserMessage model class.

---
+++ ChatClient/Models/UserMessage.cs
@@ -0,0 +1,30 @@
+﻿using SharedClasses.Domain;
+
+namespace ChatClient.Models
+{
+    /// <summary>
+    /// Represents the message information that a <see cref="Conversation"/> has.
+    /// This makes it easier to display useful information without polluting the domain model.
+    /// </summary>
+    public sealed class UserMessage
+    {
+        private readonly string message;
+        private readonly string messageDetails;
+
+        public UserMessage(string message, string messageDetails)
+        {
+            this.message = message;
+            this.messageDetails = messageDetails;
+        }
+
+        public string Message
+        {
+            get { return message; }
+        }
+
+        public string MessageDetails
+        {
+            get { return messageDetails; }
+        }
+    }
+}
