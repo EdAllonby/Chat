d2fd06b464c835333d78c08d391505dbd6db85c3
Ed Allonby <ed.allonby@blueyonder.co.uk>
2014-05-21 22:56:43 +0100

    Refactored unit tests.

---
+++ SharedClassesTests/Domain/UserRepositoryTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using SharedClasses;
+using SharedClasses.Domain;
+
+namespace SharedClassesTests.Domain
+{
+    [TestFixture]
+    public class UserRepositoryTests
+    {
+        [Test]
+        public void AddUserEntityTest()
+        {
+            var user = new User("User", 2, ConnectionStatus.Connected);
+            var userRepository = new UserRepository();
+            userRepository.AddEntity(user);
+            Assert.AreEqual(user, userRepository.FindEntityByID(user.UserId));
+        }
+
+        [Test]
+        public void AddUserEntitiesTest()
+        {
+            var user1 = new User("User", 1, ConnectionStatus.Connected);
+            var user2 = new User("User", 2, ConnectionStatus.Connected);
+            var users = new List<User> {user1, user2};
+
+            var userRepository = new UserRepository();
+
+            userRepository.AddUsers(users);
+            Assert.AreEqual(users, userRepository.GetAllEntities().ToList());
+        }
+
+        [Test]
+        public void CanNotAddSameUserEntityTwice()
+        {
+            var user = new User("User", 1, ConnectionStatus.Connected);
+
+            var userRepository = new UserRepository();
+
+            userRepository.AddEntity(user);
+            userRepository.AddEntity(user);
+
+            var users = new List<User> {user};
+
+            Assert.AreEqual(users, userRepository.GetAllEntities());
+        }
+    }
+}
