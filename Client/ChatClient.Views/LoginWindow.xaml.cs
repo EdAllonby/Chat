4ad0cf62e35ccab02bd3abbc2f316ee9cb5ac397
Ed Allonby <ed.allonby@blueyonder.co.uk>
2014-06-08 22:52:03 +0100

    Re-added LoginWindow GotFocus/LostFocus events for clearing the textbox defaults on focus.

--- Client/ChatClient.Views/LoginWindow.xaml.cs
+++ Client/ChatClient.Views/LoginWindow.xaml.cs
@@ -32,5 +32,49 @@ namespace ChatClient.Views
                 DragMove();
             }
         }
+        private void RemoveTextOnFocus(object sender, RoutedEventArgs e)
+        {
+            var focusedTextBox = (TextBox)sender;
+
+            if (focusedTextBox.Text == "Username")
+            {
+                focusedTextBox.Text = string.Empty;
+                Log.Debug("Text has been removed from LogOn textbox");
+            }
+            if (focusedTextBox.Text == "IP Address")
+            {
+                focusedTextBox.Text = string.Empty;
+                Log.Debug("Text has been removed from LogOn textbox");
+            }
+            if (focusedTextBox.Text == "Port")
+            {
+                focusedTextBox.Text = string.Empty;
+                Log.Debug("Text has been removed from LogOn textbox");
+            }
+        }
+
+        private void AddTextOnLostFocus(object sender, RoutedEventArgs e)
+        {
+            var lostFocusTextBox = (TextBox)sender;
+
+            if (string.IsNullOrEmpty(lostFocusTextBox.Text))
+            {
+                if (lostFocusTextBox.Name == "UsernameTextBox")
+                {
+                    lostFocusTextBox.Text = "Username";
+                    Log.Debug("Default text added back to textbox");
+                }
+                if (lostFocusTextBox.Name == "IPAddressTextBox")
+                {
+                    lostFocusTextBox.Text = "IP Address";
+                    Log.Debug("Default text added back to textbox");
+                }
+                if (lostFocusTextBox.Name == "PortTextBox")
+                {
+                    lostFocusTextBox.Text = "Port";
+                    Log.Debug("Default text added back to textbox");
+                }
+            }
+        }
     }
 }
