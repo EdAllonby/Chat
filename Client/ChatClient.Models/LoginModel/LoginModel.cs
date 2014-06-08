1d258c49711285cc36bb72763dd16ec604891591
Ed Allonby <ed.allonby@blueyonder.co.uk>
2014-06-08 12:21:54 +0100

    Added LoginModel class for the LoginWindowViewModel and LoginWindow in MVVM folder.

---
+++ Client/ChatClient.Models/LoginModel/LoginModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace ChatClient.Models.LoginModel
+{
+    public class LoginModel : BaseModel, IDataErrorInfo
+    {
+        private string username = "Username";
+        private string ipAddress = "IPAddress";
+        private string port = "Port";
+
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                if (value == username) return;
+                username = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string IPAddress
+        {
+            get { return ipAddress; }
+            set
+            {
+                if (value == ipAddress) return;
+                ipAddress = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Port
+        {
+            get { return port; }
+            set
+            {
+                if (value == port) return;
+                port = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                Error = String.Empty;
+
+                if (String.IsNullOrEmpty(Username))
+                {
+                    Error = "Username is required.";
+                }
+
+                if (String.IsNullOrEmpty(IPAddress))
+                {
+                    Error = "IP Address is required.";
+                }
+
+                int portInt;
+                if (!int.TryParse(Port, out portInt))
+                {
+                    Error = "Port must only consist of digits.";
+                }
+                if (portInt < 0 || portInt > 65535)
+                {
+                    Error = "Port must be between 0 and 65535.";
+                }
+
+                return Error;
+            }
+        }
+
+        public string Error { get; private set; }
+    }
+}
