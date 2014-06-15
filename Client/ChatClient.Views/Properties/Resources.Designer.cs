53f753052a2eebd2a0c2d1b540e4c54c4b1b54fc
Ed Allonby <ed.allonby@blueyonder.co.uk>
2014-06-15 23:20:25 +0100

    Revamped Login Window style.

--- Client/ChatClient.Views/Properties/Resources.Designer.cs
+++ Client/ChatClient.Views/Properties/Resources.Designer.cs
@@ -71,6 +71,16 @@ namespace ChatClient.Views.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap halftone {
+            get {
+                object obj = ResourceManager.GetObject("halftone", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
         internal static byte[] Roboto_Thin {
