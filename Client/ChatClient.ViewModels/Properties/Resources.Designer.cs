e8be0e4cf5c273578391a61e9144eb4a3d7e36ca
Ed Allonby <ed.allonby@blueyonder.co.uk>
2014-07-14 23:39:02 +0100

    Added default images and increased anti-aliasing quality.

--- Client/ChatClient.ViewModels/Properties/Resources.Designer.cs
+++ Client/ChatClient.ViewModels/Properties/Resources.Designer.cs
@@ -68,5 +68,25 @@ namespace ChatClient.ViewModels.Properties {
                 return ResourceManager.GetStream("Chat_Notification_Sound", resourceCulture);
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap DefaultDropImage {
+            get {
+                object obj = ResourceManager.GetObject("DefaultDropImage", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap DefaultUserImage {
+            get {
+                object obj = ResourceManager.GetObject("DefaultUserImage", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
