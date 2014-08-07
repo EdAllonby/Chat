38cf815ca29150e12e0692614b8ee81adaddef98
Ed Allonby <ed.allonby@blueyonder.co.uk>
2014-08-07 22:43:11 +0100

    Release mode will not show Console window and log. Application now correctly shuts down when Main Window is closed.

--- Client/ChatClient.Views/MainWindow.xaml.cs
+++ Client/ChatClient.Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Windows;
 using ChatClient.ViewModels.MainWindowViewModel;
 
 namespace ChatClient.Views
@@ -17,6 +18,14 @@ namespace ChatClient.Views
         {
             var view = new UserSettingsWindow();
             view.ShowDialog();
+        }
+
+        private void OnMainWindowClosed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+            
+            // Something is stopping the application to close gracefully. Force the application to quit.
+            Environment.Exit(0);
         }
     }
 }
