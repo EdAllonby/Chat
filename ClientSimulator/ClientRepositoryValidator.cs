90c85212c1b1b5da032c32fa79db3f7d51e072e2
Ed Allonby <edward.allonby@gmail.com>
2014-08-10 23:22:23 +0100

    Cleaned ClientSimulator Validation logging.

--- ClientSimulator/ClientRepositoryValidator.cs
+++ ClientSimulator/ClientRepositoryValidator.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Linq;
 using ChatClient.Services;
+using log4net;
 using SharedClasses.Domain;
 
 namespace ClientSimulator
@@ -9,10 +10,16 @@ namespace ClientSimulator
     /// <summary>
     /// Used to validate contents stored in repositories.
     /// </summary>
-    internal class ClientRepositoryValidator
+    internal static class ClientRepositoryValidator
     {
-        public static void ValidateUserRepository(IReadOnlyCollection<IClientService> clients)
+        private static readonly ILog Log = LogManager.GetLogger(typeof (ClientRepositoryValidator));
+
+        public static bool ValidateUserRepository(IReadOnlyCollection<IClientService> clients)
         {
+            bool areRepositoriesValid = true;
+
+            Log.Info("Starting User Repository Validator");
+            Log.Info("===================================");
             int totalClients = clients.Count;
 
             int userIdSum = 0;
@@ -20,6 +27,7 @@ namespace ClientSimulator
             for (int id = 0; id <= totalClients; id++)
             {
                 userIdSum += id;
+                Log.InfoFormat("Total client count: {0}", totalClients);
             }
 
             int sum = clients.Sum(client => client.ClientUserId);
@@ -28,9 +36,18 @@ namespace ClientSimulator
 
             foreach (UserRepository userRepository in clients.Select(client => client.RepositoryManager.UserRepository))
             {
-                sum = userRepository.GetAllEntities().Sum(user => user.Id);
-                Debug.Assert(sum == userIdSum);
+                int userCount = userRepository.GetAllEntities().Count();
+
+                Log.InfoFormat("Total users in repository count: {0}", userCount);
+
+                if (userCount != totalClients)
+                {
+                    Log.Error("User Repository invalid");
+                    areRepositoriesValid = false;
+                    break;
+                }
             }
+            return areRepositoriesValid;
         }
     }
 }
