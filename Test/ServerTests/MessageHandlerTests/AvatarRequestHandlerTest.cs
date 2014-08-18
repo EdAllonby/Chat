12e7a777e8207fc0e0d063e0384f4811f204417c
Ed Allonby <ed.allonby@blueyonder.co.uk>
2014-08-18 23:01:48 +0100

    Added AvatarRequestHandler tests

---
+++ Test/ServerTests/MessageHandlerTests/AvatarRequestHandlerTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using NUnit.Framework;
+using Server;
+using Server.MessageHandler;
+using ServerTests.Properties;
+using SharedClasses;
+using SharedClasses.Domain;
+using SharedClasses.Message;
+
+namespace ServerTests.MessageHandlerTests
+{
+    [TestFixture]
+    public class AvatarRequestHandlerTest
+    {
+        private const int UserId = 1;
+
+        private AvatarRequest avatarRequest;
+        private User userToUpdate;
+        private RepositoryManager repositoryManager;
+        private IServerMessageContext serverMessageContext;
+        private AvatarRequestHandler avatarRequestHandler;
+
+        private UserRepository UserRepository
+        {
+            get { return repositoryManager.UserRepository; }
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            avatarRequest = new AvatarRequest(UserId, Resources.SmallImage);
+            userToUpdate = new User("user", UserId, new ConnectionStatus(UserId, ConnectionStatus.Status.Connected));
+            repositoryManager = new RepositoryManager();
+            repositoryManager.UserRepository.AddEntity(userToUpdate);
+            serverMessageContext = new ServerMessageContext(new ClientManager(), new EntityIdAllocatorFactory(), repositoryManager);
+            avatarRequestHandler = new AvatarRequestHandler();
+        }
+
+        [TestFixture]
+        public class HandleMessage : AvatarRequestHandlerTest
+        {
+            [Test]
+            public void UserGetsUpdatedAvatarInUserRepository()
+            {
+                avatarRequestHandler.HandleMessage(avatarRequest, serverMessageContext);
+
+                Assert.IsTrue(UserRepository.FindEntityById(UserId).Avatar.UserAvatar.Size.Equals(Resources.SmallImage.Size));
+            }
+
+            [Test]
+            public void AvatarGetsAssignedId()
+            {
+                avatarRequestHandler.HandleMessage(avatarRequest, serverMessageContext);
+
+                Assert.IsTrue(UserRepository.FindEntityById(UserId).Avatar.Id != 0);
+            }
+
+            [Test]
+            public void UserRepositoryUpdatesUser()
+            {
+                bool isUserUpdated = false;
+                UserRepository.EntityUpdated += (sender, eventArgs) => isUserUpdated = true;
+
+                avatarRequestHandler.HandleMessage(avatarRequest, serverMessageContext);
+
+                Assert.IsTrue(isUserUpdated);
+            }
+
+            [Test]
+            public void ThrowsExceptionWhenNonAvatarRequestIsPassed()
+            {
+                Assert.Throws<InvalidCastException>(() => avatarRequestHandler.HandleMessage(new LoginRequest("login"), serverMessageContext));
+            }
+        }
+    }
+}
--- Test/ServerTests/Properties/AssemblyInfo.cs
+++ Test/ServerTests/Properties/AssemblyInfo.cs
@@ -1,17 +1,15 @@
-﻿// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using log4net.Config;
 
-[assembly: AssemblyTitle("Server")]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ServerTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Server")]
+[assembly: AssemblyProduct("ServerTests")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,16 +17,10 @@ using log4net.Config;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("8116a546-b7f2-4248-bfef-52c95bb77cae")]
-
-// Set the project to watch the log4net.config file and reload of the config changes when needed.
-
-[assembly: XmlConfigurator(ConfigFile = "log4net.config", Watch = true)]
+[assembly: Guid("e1f6905e-02c2-42ad-9047-38d7f41704be")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -40,8 +32,5 @@ using log4net.Config;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-
-[assembly:InternalsVisibleTo("ServerTests")]
