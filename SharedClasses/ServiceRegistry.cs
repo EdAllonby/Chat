f175b7d96646c02f4fc05416c3454759649aa683
Ed Allonby <ed.allonby@blueyonder.co.uk>
2014-08-21 23:07:41 +0100

    Added summaries to new classes/interfaces.

--- SharedClasses/ServiceRegistry.cs
+++ SharedClasses/ServiceRegistry.cs
@@ -4,11 +4,20 @@ using log4net;
 
 namespace SharedClasses
 {
+    /// <summary>
+    /// Holds references to services.
+    /// </summary>
     public sealed class ServiceRegistry : IServiceRegistry
     {
         private static readonly ILog Log = LogManager.GetLogger(typeof (ServiceRegistry));
+
         private readonly IDictionary<Type, IService> services = new Dictionary<Type, IService>();
 
+        /// <summary>
+        /// Gets a service from its type.
+        /// </summary>
+        /// <typeparam name="T">The type of service to return.</typeparam>
+        /// <returns>The service requested.</returns>
         public T GetService<T>() where T : IService
         {
             if (!services.ContainsKey(typeof (T)))
@@ -19,6 +28,11 @@ namespace SharedClasses
             return (T) services[typeof (T)];
         }
 
+        /// <summary>
+        /// Adds a service to the registry.
+        /// </summary>
+        /// <typeparam name="T">The type of service to add.</typeparam>
+        /// <param name="service">The service instance to add.</param>
         public void RegisterService<T>(IService service) where T : IService
         {
             services.Add(typeof (T), service);
